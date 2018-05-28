dc7940de851034aada9202eaaf376e38074987f7
Magnus Enger <magnus@libriotech.no>
2018-05-28 09:09:15 +0000

    Change 001 of sample record, make reset.sql use it

--- data/reset.sql
+++ data/reset.sql
@@ -1,4 +1,3 @@
 -- Run this to remove the data added by testing
 
-DELETE FROM items WHERE biblionumber = 3;
-DELETE FROM biblio where biblionumber = 3;
+DELETE FROM biblio WHERE biblionumber IN ( SELECT biblionumber FROM biblio_metadata WHERE ExtractValue( metadata, '//controlfield[@tag="001"]' ) = 'MySpecial001' );
