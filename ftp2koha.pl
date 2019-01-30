aa455912b955fbe2cc462b9895d773c46704d5fe
Magnus Enger <magnus@libriotech.no>
2019-01-30 14:12:02 +0100

    Comment a debug statement

--- ftp2koha.pl
+++ ftp2koha.pl
@@ -113,7 +113,7 @@ while ( my $record = $records->next() ) {
     my $sth = $dbh->prepare("SELECT biblionumber, metadata FROM biblio_metadata WHERE ExtractValue( metadata, '//controlfield[\@tag=\"001\"]' ) = '$id_001'");
     $sth->execute();
     my $hits = $sth->fetchall_arrayref;
-    print Dumper $hits if $debug;
+    # print Dumper $hits if $debug;
 
     my $itemdetails = '';
 
