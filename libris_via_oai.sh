89ac9232605fbdf9ec5b312e75f4b6a132789b97
Magnus Enger <magnus@libriotech.no>
2020-05-13 14:37:25 +0200

    Add libris_via_oai.sh

---
+++ libris_via_oai.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This script is based on the example script provided by Libris:
+# https://github.com/libris/export/blob/master/examplescripts/export_nix.sh
+# See this document for details on how to setup and configure everything:
+# https://github.com/libris/librisxl/blob/master/marc_export/marc-export-documentation.md
+# ---
+
+# Det här skriptet kan användas som exempel på hur man automatiskt hämtar poster från Libris
+# Innan du använder det, se till att du fyllt i filen: etc/export.properties
+#
+# Lämpligen körs detta skript minut-vis m h a cron.
+
+set -e
+
+# Se till att vi inte kör flera instanser av skriptet samtidigt
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
+
+# Om vi kör för första gången, sätt 'nu' till start-tid
+LASTRUNTIMEPATH="lastRun.timestamp"
+if [ ! -e $LASTRUNTIMEPATH ]
+then
+    date -u +%Y-%m-%dT%H:%M:%SZ > $LASTRUNTIMEPATH
+fi
+
+# Avgör vilket tidsintervall vi ska hämta
+STARTTIME=`cat $LASTRUNTIMEPATH`
+STOPTIME=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+OUTFILE="export-$STOPTIME.marcxml"
+
+# Hämta data
+curl --silent --fail -XPOST "https://libris.kb.se/api/marc_export/?from=$STARTTIME&until=$STOPTIME&deleted=ignore&virtualDelete=false" --data-binary @./export.properties > $OUTFILE
+
+# Om allt gick bra, uppdatera tidsstämpeln
+echo $STOPTIME > $LASTRUNTIMEPATH
+
+MAXSIZE=105
+ACTUALSIZE=$(wc -c <"$OUTFILE")
+if [ $ACTUALSIZE -le $MAXSIZE ]; then
+    # Too small, delete it
+    rm "$OUTFILE"
+    # echo "Deleted $OUTFILE, $ACTUALSIZE bytes"
+else
+    # Process it 
+    echo "Wrote $OUTFILE, $ACTUALSIZE bytes"
+fi
